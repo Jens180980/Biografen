365d0d0d62e283d665b069261e9271b910c295a5
Eva-Renata <all2evi@yahoo.com>
2021-10-12 13:09:25 +0200

    tirdagsarbejde

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,33 @@ window.addEventListener("load", function(){
     let loader = document.querySelector(".loader");
     loader.classList += " hidden";
 });
+
+//vi kan se mere tekst ved at klikke på
+function toggleExpand(link) { 
+    const element = document.querySelector('.toggleText');
+    element.classList.toggle('texthidden');
+    if(link.innerText === '↓ Læs videre'){
+        link.innerText = '↑ Læs mindre'
+    }else {
+        link.innerText = '↓ Læs videre'
+    }
+  }
+
+
+  //play video
+  const videoPlayer = document.querySelector('.video');
+  // VIDEO play and stop
+  function playVideo(videolink){
+      const video = document.createElement('video')
+      video.src = videolink;
+      video.setAttribute('controls','controls');
+      videoPlayer.prepend(video);
+      videoPlayer.classList.add('active');
+  }
+
+  //closing the video
+  function closeVideo(){
+    const video = document.querySelector('video');
+    video.remove();
+    videoPlayer.classList.remove('active');
+  }
